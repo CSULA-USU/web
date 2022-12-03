a1274b5c95db1f1fb0c9d7dbc86a8f203ff65df2
John Yasis <johnamielyasis@gmail.com>
2022-12-02 16:28:57 -0800

    adds filler text to rec webpage

--- src/pages/recreation/index.tsx
+++ src/pages/recreation/index.tsx
@@ -4,21 +4,30 @@ import Image from 'next/image'
 import {
     Page,
 } from 'modules';
+import { Typography } from 'components';
 
 const RecreationContainer = styled.div`
-    background-color: red;
-    height: 1000px;
-    max-width: 100%;
+    height: 2000px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
 `
 
 const RecreationHeroContainer = styled.div`
     background: url('/recreation-hero-background.jpg') no-repeat;
     background-size: cover;
-    height: 640px;
+    height: 960px;
     max-width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
+`
+
+const RecreationInnerContainer = styled.div`
+    height: 800px;
+    max-width: 1000px;
+    text-align: center;
+    margin: 0 auto;
 `
 export default function Home() {
     return (
@@ -40,6 +49,35 @@ export default function Home() {
                 <RecreationHeroContainer>
                     <Image src="/recreation-hero.png" alt="recreation logo" width="660" height="258" />
                 </RecreationHeroContainer>
+                <RecreationInnerContainer>
+                    <Typography as="h2" variant="bodySerif" size="lg" weight="500" margin="24px 0 24px">
+                        The Recreation Fitness Center is now open!
+                        <br />
+                        Join us in our renovated facility:
+                        <br />
+                        Monday through Friday from 7:00 a.m. to 9:30 p.m., and Saturday 7:00 a.m. to 2:30 p.m.!
+                    </Typography>
+                    <Typography as="p" variant="bodySerif" margin="24px 0 24px">
+                        Recreation at Cal State LA provides Golden Eagles with opportunities to play,
+                        exercise and engage their campus community through programming and events aimed
+                        toward enhancing the experience of all who participate.
+                        <br />
+                        Recreation is comprised of the Recreation Fitness Center Center and Recreation
+                        Esports. The Recreation Fitness Center is located on the basement level of the
+                        U-SU, and will be open to all students, staff and faculty.
+                    </Typography>
+                    <div>
+                        <Typography as="p" variant="bodySerif">
+                            <strong>Renovations:</strong>
+                            <ul>
+                                <li>New rubber flooring in the fitness center</li>
+                                <li>All new strength equipment</li>
+                                <li>Expansion of fitness into the former games room.</li>
+                            </ul>
+                            For updates and ways to connect now, follow @calstatela_recreation on Instagram!
+                        </Typography>
+                    </div>
+                </RecreationInnerContainer>
             </RecreationContainer>
         </Page>
     );
