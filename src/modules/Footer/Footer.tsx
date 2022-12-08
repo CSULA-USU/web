39dcf74ead7b93c90b82ad3a4adab0bc0dfd45b6
Mac 17 <amanese2@calstatela.edu>
2022-12-08 15:36:06 -0800

    chore: adds fluid container to footer

--- src/modules/Footer/Footer.tsx
+++ src/modules/Footer/Footer.tsx
@@ -2,12 +2,7 @@ import Image from 'next/image';
 import styled from 'styled-components';
 import { Typography } from 'components';
 import { Colors } from 'theme';
-import Link from 'next/link';
-
-const FooterContainer = styled.div`
-  padding: 52px 52px 22px 52px;
-  background-color: ${Colors.greyDarkest};
-`;
+import { FluidContainer } from 'components';
 
 const FooterInner = styled.div`
   display: flex;
@@ -35,66 +30,64 @@ const FooterTopContainer = styled.div`
   justify-content: center;
 `;
 export const Footer = () => (
-  <>
-    <FooterContainer>
-      <FooterTopContainer>
-        <FooterInner>
-          <Image
-            src="/calstatela-badge.svg"
-            alt="Cal State LA Univeristy-Student Union Logo"
-            width={90}
-            height={90}
-          />
+  <FluidContainer backgroundColor='greyDarkest'>
+    <FooterTopContainer>
+      <FooterInner>
+        <Image
+          src="/calstatela-badge.svg"
+          alt="Cal State LA Univeristy-Student Union Logo"
+          width={90}
+          height={90}
+        />
+        <div>
+          <Typography as="h5" variant="smallHeading" color="white">
+            Involvment
+          </Typography>
+          <Typography color="primary" size="sm" lineHeight="2.4">
+            U-SU Employment
+          </Typography>
+          <Typography color="primary" size="sm" lineHeight="2.4">
+            Calendar of Events
+          </Typography>
+        </div>
+        <div>
+          <Typography as="h5" variant="smallHeading" color="white">
+            Resources
+          </Typography>
+          <Typography color="primary" size="sm" lineHeight="2.4">
+            Cal State L.A. Chancellor Message against racism
+          </Typography>
+          <Typography color="primary" size="sm" lineHeight="2.4">
+            President Covino&apos;s message against racism
+          </Typography>
+          <Typography color="primary" size="sm" lineHeight="2.4">
+            Black Lives Matter Resources
+          </Typography>
           <div>
-            <Typography as="h5" variant="smallHeading" color="white">
-              Involvment
+            <Typography color="primary" size="sm" lineHeight="2.4">
+              Covid-19 Resources
             </Typography>
             <Typography color="primary" size="sm" lineHeight="2.4">
-              U-SU Employment
-            </Typography>
-            <Typography color="primary" size="sm" lineHeight="2.4">
-              Calendar of Events
+              View Public Documents
             </Typography>
           </div>
-          <div>
-            <Typography as="h5" variant="smallHeading" color="white">
-              Resources
-            </Typography>
-            <Typography color="primary" size="sm" lineHeight="2.4">
-              Cal State L.A. Chancellor Message against racism
-            </Typography>
-            <Typography color="primary" size="sm" lineHeight="2.4">
-              President Covino&apos;s message against racism
-            </Typography>
-            <Typography color="primary" size="sm" lineHeight="2.4">
-              Black Lives Matter Resources
-            </Typography>
-            <div>
-              <Typography color="primary" size="sm" lineHeight="2.4">
-                Covid-19 Resources
-              </Typography>
-              <Typography color="primary" size="sm" lineHeight="2.4">
-                View Public Documents
-              </Typography>
-            </div>
-          </div>
-        </FooterInner>
-      </FooterTopContainer>
-      <Border></Border>
-      <FooterBottomContainer>
-        <Typography color="greyLighter" size="sm" lineHeight="2.4">
-          © 2022 University-Student Union at Cal State LA
-        </Typography>
-        <Typography color="greyLighter" size="sm" lineHeight="2.4">
-          |
-        </Typography>
-        <Typography color="greyLighter" size="sm" lineHeight="2.4">
-          5154 State University Dr, Los Angeles, CA 90032
-        </Typography>
-        <Typography color="greyLighter" size="sm" lineHeight="2.4">
-          (323) 343-2465
-        </Typography>
-      </FooterBottomContainer>
-    </FooterContainer>
-  </>
+        </div>
+      </FooterInner>
+    </FooterTopContainer>
+    <Border></Border>
+    <FooterBottomContainer>
+      <Typography color="greyLighter" size="sm" lineHeight="2.4">
+        © 2022 University-Student Union at Cal State LA
+      </Typography>
+      <Typography color="greyLighter" size="sm" lineHeight="2.4">
+        |
+      </Typography>
+      <Typography color="greyLighter" size="sm" lineHeight="2.4">
+        5154 State University Dr, Los Angeles, CA 90032
+      </Typography>
+      <Typography color="greyLighter" size="sm" lineHeight="2.4">
+        (323) 343-2465
+      </Typography>
+    </FooterBottomContainer>
+  </FluidContainer>
 );
