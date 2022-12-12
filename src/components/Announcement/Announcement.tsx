a46c49fd362c5ac2a1dfc1c9cb4ed7823b918e3f
John Yasis <johnamielyasis@gmail.com>
2022-12-12 15:31:31 -0800

    feat: add megaphone icon and typography styling

--- src/components/Announcement/Announcement.tsx
+++ src/components/Announcement/Announcement.tsx
@@ -1,23 +1,34 @@
 import { Colors } from 'theme';
 import styled from 'styled-components';
 import { Typography } from 'components';
+import { IconContext } from 'react-icons';
+import { BsMegaphone } from 'react-icons/bs';
 interface AnnouncementProps {
     announcementText: string;
 }
 
 const AnnouncementContainer = styled.div`
-    font-size: 16px;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 40px;
+    height: 48px;
     background-color: ${Colors.primary};
 `
 
 export const Announcement = ({ announcementText }: AnnouncementProps) => {
     return (
         <AnnouncementContainer>
-            <Typography as="h2" variant="smallHeading">
+                <IconContext.Provider value={{
+                    color: 'black',
+                    style: {
+                        height: '24px',
+                        width: '27px',
+                        margin: 'auto 16 auto 0',
+                    }
+                }}>
+                    <BsMegaphone />
+                </IconContext.Provider>
+            <Typography as="h2" variant="bodySerif" weight="500">
                 {announcementText}
             </Typography>
         </AnnouncementContainer>
