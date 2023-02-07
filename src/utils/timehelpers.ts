13fa1a2fa06b22ab73da47719fb536b57c832955
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2023-02-07 09:53:11 -0800

    fix: make format required in time helpers

--- src/utils/timehelpers.ts
+++ src/utils/timehelpers.ts
@@ -6,12 +6,12 @@ export const getTime = (utc: string) =>
 
 export const getMonth = (
   utc: string,
-  format?: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow' = 'long',
+  format: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow' = 'long',
 ) => new Date(utc).toLocaleString('default', { month: format });
 
 export const getDay = (utc: string) => new Date(utc).getDate();
 
 export const getYear = (
   utc: string,
-  format?: 'numeric' | '2-digit' = 'numeric',
+  format: 'numeric' | '2-digit' = 'numeric',
 ) => new Date(utc).toLocaleString('default', { year: format });
