b66e2db5660041c2a3d78a3c9ada1b41b49209ee
Mac 17 <amanese2@calstatela.edu>
2023-02-22 08:49:29 -0800

    feat: adds modal to events for department pages

--- src/modules/DepartmentHeader/DepartmentHeader.tsx
+++ src/modules/DepartmentHeader/DepartmentHeader.tsx
@@ -3,6 +3,9 @@ import { EventCard } from '../../modules/EventCard';
 import styled from 'styled-components';
 import { useRecoilValue } from 'recoil';
 import { eventListState } from 'atoms';
+import { useState } from 'react';
+import { PresenceEvent } from 'types';
+import { EventModal } from 'modules/EventModal';
 
 interface DepartmentHeaderProps {
   title: string;
@@ -27,20 +30,87 @@ export const DepartmentHeader = ({
   const departmentEvent = events.find(
     (event) => event.organizationName === title,
   );
-
+  const [selectedEvent, selectEvent] = useState<undefined | PresenceEvent>(
+    undefined,
+  );
+  const onRequestClose = () => selectEvent(undefined);
   return (
-    <FluidContainer backgroundImage="subtle-background-3.jpg">
-      <HeaderContainer>
-        <HeaderContent>
-          <Typography variant="labelTitle">Welcome to the</Typography>
-          <Typography margin="0 0 24px" variant="titleLarge" weight="400">
-            {title}
-          </Typography>
-          <Typography>{children}</Typography>
-        </HeaderContent>
-        {departmentEvent && <EventCard featured event={departmentEvent} />}
-      </HeaderContainer>
-      {infoSection}
-    </FluidContainer>
+    <>
+      <FluidContainer backgroundImage="subtle-background-3.jpg">
+        <HeaderContainer>
+          <HeaderContent>
+            <Typography variant="labelTitle">Welcome to the</Typography>
+            <Typography margin="0 0 24px" variant="titleLarge" weight="400">
+              {title}
+            </Typography>
+            <Typography>{children}</Typography>
+          </HeaderContent>
+          {departmentEvent && (
+            <EventCard
+              onClick={() => selectEvent(departmentEvent)}
+              featured
+              event={departmentEvent}
+            />
+          )}
+        </HeaderContainer>
+        {infoSection}
+      </FluidContainer>
+      <EventModal
+        isOpen={!!selectedEvent}
+        event={selectedEvent}
+        onRequestClose={onRequestClose}
+      />
+    </>
   );
 };
+
+// import styled from 'styled-components';
+// import { FluidContainer, Typography } from 'components';
+// import { EventCard } from 'modules';
+// import { PresenceEvent } from 'types';
+// import { Spaces } from 'theme';
+// import { useState } from 'react';
+// import { EventModal } from 'modules/EventModal';
+
+// const HeaderContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+// `;
+
+// export const EventHeader = ({
+//   featuredEvent,
+//   title,
+// }: {
+//   featuredEvent: PresenceEvent;
+//   title?: React.ReactNode;
+// }) => {
+//   const [selectedEvent, selectEvent] = useState<undefined | PresenceEvent>(
+//     undefined,
+//   );
+//   const onRequestClose = () => selectEvent(undefined);
+//   return (
+//     <FluidContainer
+//       flex
+//       flexDirection="column"
+//       backgroundImage="/subtle-background-1.jpg"
+//     >
+//       <Typography
+//         variant="pageHeader"
+//         margin={`0 auto ${Spaces.lg}`}
+//         size="2xl"
+//         color="greyDarker"
+//       >
+//         {title}
+//       </Typography>
+//       <HeaderContainer onClick={() => selectEvent(featuredEvent)}>
+//         <EventCard featured event={featuredEvent} />
+//       </HeaderContainer>
+//       <EventModal
+//         isOpen={!!selectedEvent}
+//         event={selectedEvent}
+//         onRequestClose={onRequestClose}
+//       />
+//     </FluidContainer>
+//   );
+// };
