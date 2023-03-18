ac0c73651f34d1b8aed1a1ff3dc0e9416b42c8f4
John Yasis <johnamielyasis@gmail.com>
2023-03-17 18:32:54 -0700

    add vector and cta to apisrc page

--- src/pages/ccc/apisrc/index.tsx
+++ src/pages/ccc/apisrc/index.tsx
@@ -34,37 +34,33 @@ const offerings = [
     title: 'Cultural Education',
     children:
       'Cultural Education – Provide scholarly and cultural education programs. Approaching cultural diversity from an academic perspective that provides the entire campus community with an opportunity to culturally engage and learn outside of the classroom.',
-    imgSrc: '/departments/ccc/apisrcHeader.png',
+    imgSrc: '/vectors/ccc/teaching.svg',
     imgAlt: 'cultural education',
     href: 'ccc/apisrc',
-    linkText: 'Learn More',
   },
   {
     title: 'Cultural Engagement',
     children:
       'Cultural Engagement – Provide opportunities for students, staff, faculty, and community members to be part of the practice, celebration, and demonstration of cultural celebration and joy',
-    imgSrc: '/departments/ccc/apisrcHeader.png',
+    imgSrc: '/vectors/ccc/winner.svg',
     imgAlt: 'apisrc',
     href: 'ccc/apisrc',
-    linkText: 'Learn More',
   },
   {
     title: 'Cultural Student Development',
     children:
       'Cultural Student Development – Provide students with opportunities to develop their academic, professional, and personal growth during their undergraduate experience',
-    imgSrc: '/departments/ccc/apisrcHeader.png',
+    imgSrc: '/vectors/ccc/education.svg',
     imgAlt: 'apisrc',
     href: 'ccc/apisrc',
-    linkText: 'Learn More',
   },
   {
     title: 'Cultural Environment Enhancement',
     children:
       'Cultural Environment Enhancement – Provide a safe space on campus for APIDA-identified students where they see themselves reflected, embraced, celebrated, and validated. Resources available within the center',
-    imgSrc: '/departments/ccc/apisrcHeader.png',
+    imgSrc: '/vectors/ccc/reading-lounge.svg',
     imgAlt: 'apisrc',
     href: 'ccc/apisrc',
-    linkText: 'Learn More',
   },
 ];
 
@@ -169,14 +165,16 @@ export default function APISRC() {
         href="https://forms.office.com/r/2pTsmaFvCk"
         buttonText="Sign Up"
         text="APIDA Grad Committee"
+        backgroundColorProp="primary"
       >
-        <Typography as="h2" variant="label">
+        <Typography as="h3" variant="labelTitle">
           We&apos;re looking for committee members to help us plan APIDA Grad!
           From the theme, sash design, performance - join us in making one of
           the biggest celebrations happen! You don&apos;t have to be a
           graduating student to be on the committee!
         </Typography>
       </CallToAction>
+
       <FluidContainer>
         <Typography margin="24px 8px 24px 0px" as="h2" variant="titleSmall">
           The APISRC continues to serve the mission through 4 components:
