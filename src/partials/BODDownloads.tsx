c9bab68e99c9fd6856ff3def64564e15def030a8
Justin <128175988+Justin-Fournier@users.noreply.github.com>
2023-03-23 14:38:30 -0700

    chore:add bod agenda and minutes pdfs

--- src/partials/BODDownloads.tsx
+++ src/partials/BODDownloads.tsx
@@ -21,47 +21,28 @@ const agendaDownloads: DownloadSectionProps[] = [
         stacked
         links={[
           {
-            href: '/governance/public-documents/agenda/bod-agenda-fy22-23-080122.pdf',
+            href: '/governance/public-documents/agenda/fy22-23/bod-aug-01-22.pdf',
             children: 'BOD Agenda August 1, 2022',
           },
           {
-            href: '/governance/public-documents/agenda/bod-agenda-fy22-23-091622.pdf',
+            href: '/governance/public-documents/agenda/fy22-23/bod-sep-16-22.pdf',
             children: 'BOD Agenda September 16, 2022',
           },
           {
-            href: '/governance/public-documents/agenda/bod-agenda-fy22-23-093022.pdf',
+            href: '/governance/public-documents/agenda/fy22-23/bod-sep-30-22.pdf',
             children: 'BOD Agenda September 30, 2022',
           },
           {
-            href: '/governance/public-documents/agenda/bod-agenda-fy22-23-012023.pdf',
+            href: '/governance/public-documents/agenda/fy22-23/bod-jan-20-23.pdf',
             children: 'BOD Agenda January 20, 2023',
           },
-        ]}
-      />
-    ),
-    button: {
-      children: <NonBreakingSpan>Download All</NonBreakingSpan>,
-      href: '#',
-      variant: 'black',
-    },
-  },
-  {
-    title: 'Fiscal Year 21-22',
-    children: (
-      <DocumentLinkContainer
-        stacked
-        links={[
           {
-            href: '/governance/public-documents/agenda/bod-agenda-fy-21-22-012122.pdf',
-            children: 'BOD Agenda January 21, 2022',
+            href: '/governance/public-documents/agenda/fy22-23/bod-feb-10-23.pdf',
+            children: 'BOD Agenda February 10, 2023',
           },
           {
-            href: '/governance/public-documents/agenda/bod-agenda-fy21-22-101521.pdf',
-            children: 'BOD Agenda October 15, 2021',
-          },
-          {
-            href: '/governance/public-documents/agenda/bod-agenda-fy21-22-110521.pdf',
-            children: 'BOD Agenda November 5, 2021',
+            href: '/governance/public-documents/agenda/fy22-23/bod-mar-10-23.pdf',
+            children: 'BOD Agenda March 10, 2023',
           },
         ]}
       />
@@ -80,9 +61,26 @@ const minutesDownloads: DownloadSectionProps[] = [
       <DocumentLinkContainer
         stacked
         links={[
-          { href: '#', children: 'BOD Minutes August 1, 2022' },
-          { href: '#', children: 'BOD Minutes September 30, 2022' },
-          { href: '#', children: 'BOD Minutes September 16, 2022' },
+          {
+            href: '/governance/public-documents/minutes/fy22-23/bod-may-13-22.pdf',
+            children: 'BOD Minutes May 13, 2022',
+          },
+          {
+            href: '/governance/public-documents/minutes/fy22-23/bod-aug-01-22.pdf',
+            children: 'BOD Minutes August 1, 2022',
+          },
+          {
+            href: '/governance/public-documents/minutes/fy22-23/bod-sep-16-22.pdf',
+            children: 'BOD Minutes September 16, 2022',
+          },
+          {
+            href: '/governance/public-documents/minutes/fy22-23/bod-oct-14-22.pdf',
+            children: 'BOD Minutes October 14, 2022',
+          },
+          {
+            href: '/governance/public-documents/minutes/fy22-23/bod-feb-10-23.pdf',
+            children: 'BOD Minutes February 10, 2022',
+          },
         ]}
       />
     ),
@@ -96,16 +94,7 @@ const minutesDownloads: DownloadSectionProps[] = [
 const meetingsDownloads: DownloadSectionProps[] = [
   {
     title: 'Fiscal Year 22-23',
-    children: (
-      <DocumentLinkContainer
-        stacked
-        links={[
-          { href: '#', children: 'BOD Meeting August 1, 2022' },
-          { href: '#', children: 'BOD Meeting September 30, 2022' },
-          { href: '#', children: 'BOD Meeting September 16, 2022' },
-        ]}
-      />
-    ),
+    children: <DocumentLinkContainer stacked links={[]} />,
     button: {
       children: <NonBreakingSpan>Download All</NonBreakingSpan>,
       href: '#',
