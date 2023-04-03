3c728a81c8da0977835b6cce6874604d1375c193
Justin <128175988+Justin-Fournier@users.noreply.github.com>
2023-04-03 15:01:52 -0700

    chore:add gene pdf link

--- src/pages/recreation/gene.tsx
+++ src/pages/recreation/gene.tsx
@@ -35,7 +35,7 @@ const cards = [
 
 const buttons = [
   { text: 'Linktree', href: 'https://linktr.ee/CalStateLA_Recreation' },
-  { text: 'Meet Your Educators', href: '#' },
+  { text: 'Meet Your Educators', href: '/gene-bios.pdf' },
 ];
 
 export default function Gene() {
