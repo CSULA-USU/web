b8dd985d0a10c53cf3f2afb1bc77c62fe4775378
Mac 17 <amanese2@calstatela.edu>
2023-04-19 13:39:24 -0700

    feat: adds u-krew images to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,22 @@ import {
   UpcomingEvents,
   BoardOfDirectorsCTA,
   Page,
-  CallToAction,
 } from 'modules';
-import { NonBreakingSpan, Typography } from 'components';
+import {
+  NonBreakingSpan,
+  Typography,
+  FluidContainer,
+  Button,
+  Image,
+} from 'components';
 import { useRecoilValue } from 'recoil';
 import { eventListState } from 'atoms';
 import { useBreakpoint } from 'hooks';
+import { Spaces } from 'theme';
 
 export default function Home() {
   const events = useRecoilValue(eventListState);
-  const { isMobile } = useBreakpoint();
+  const { isMobile, isDesktop } = useBreakpoint();
   return (
     <Page>
       <Head>
@@ -48,16 +54,55 @@ export default function Home() {
         featuredEvent={events[0]}
       />
       <UpcomingEvents events={events} />
-      <CallToAction
-        buttonText="View Opportunities"
-        text="Catalyze your professional development and build your network by
-          becoming a valued member of the U-SU."
-        href="/employment"
-      >
-        <Typography as="h2" variant="label">
-          Join the U-Krew!
-        </Typography>
-      </CallToAction>
+      <FluidContainer flex backgroundColor="primary" padding="0">
+        <FluidContainer>
+          <Typography as="h2" variant="titleLarge" lineHeight="1">
+            Join the
+            <br />
+            <strong>U-Krew!</strong>
+          </Typography>
+          <Typography as="p" margin={`${Spaces.md} 0`}>
+            Catalyze your professional development and build your network by
+            becoming a valued member of the U-SU.
+          </Typography>
+          <Button href="/employment" variant="black">
+            View Opportunities
+          </Button>
+        </FluidContainer>
+        {!isDesktop && (
+          <FluidContainer
+            flex
+            flexWrap="wrap"
+            justifyContent="space-evenly"
+            backgroundColor="white"
+          >
+            <Image
+              src="/departments/operations/images/building-maintenance.jpg"
+              alt="building-maintenance"
+              width="275px"
+              margin="6px"
+            />
+            <Image
+              src="/departments/operations/images/information-event-services.jpg"
+              alt="building-maintenance"
+              width="275px"
+              margin="6px"
+            />
+            <Image
+              src="/departments/operations/images/building-services.jpg"
+              alt="building-maintenance"
+              width="275px"
+              margin="6px"
+            />
+            <Image
+              src="/departments/operations/images/media-services.jpg"
+              alt="building-maintenance"
+              width="275px"
+              margin="6px"
+            />
+          </FluidContainer>
+        )}
+      </FluidContainer>
       <BoardOfDirectorsCTA />
     </Page>
   );
