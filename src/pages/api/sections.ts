7bf8df9760046304bcbe5bbaa65c9af77215060b
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2023-05-18 10:59:42 -0700

    fix: move lib folder out of pages

--- src/pages/api/sections.ts
+++ src/pages/api/sections.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from 'pages/lib/supabase';
+import { supabase } from 'lib/supabase';
 
 export default async function handler(
   req: NextApiRequest,
