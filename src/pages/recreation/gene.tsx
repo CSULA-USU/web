a4b9644e397135866744f16e6379090e147d5bda
Justin <jfournier996@gmail.com>
2023-08-16 13:31:02 -0700

    chore: program portfolio button pdf fix

--- src/pages/recreation/gene.tsx
+++ src/pages/recreation/gene.tsx
@@ -36,7 +36,7 @@ const cards = [
 const buttons = [
   {
     text: 'Program Portfolio',
-    href: 'https://www.dropbox.com/scl/fi/umgxniy3fxfocj60vmxja/ACHA_Poster_Final_5.31.22.pdf?rlkey=hel3lzj55v8tuzao50fvu66y1&dl=0',
+    href: 'https://www.dropbox.com/scl/fi/umgxniy3fxfocj60vmxja/ACHA_Poster_Final_5.31.22.pdf?rlkey=hel3lzj55v8tuzao50fvu66y1&raw=1',
   },
   {
     text: 'Meet Your Educators',
