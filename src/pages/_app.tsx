d55c179245cc938e09e7a4b604c5ce1e19c98d50
John Yasis <johnamielyasis@gmail.com>
2023-09-20 10:06:19 -0700

    feat: add ga-4 library to initialize google analytics 4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,15 @@
 import 'styles/globals.css';
-import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import { EventsLoader } from 'components';
 import { RecoilRoot } from 'recoil';
+import ReactGA from 'react-ga4';
+import { EventsLoader } from 'components';
 
 export default function App({ Component, pageProps }: AppProps) {
+  ReactGA.initialize(`${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`);
   return (
     <RecoilRoot>
       <EventsLoader />
-      <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        ></script>
-      </Head>
       <Component {...pageProps} />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-          `,
-        }}
-      />
     </RecoilRoot>
   );
 }
