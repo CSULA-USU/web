04af5b565f25a6c1541d51e3e2f380240e3284ef
John Yasis <johnamielyasis@gmail.com>
2023-10-20 16:14:39 -0700

    chore: add drop shadow styling to nav

--- src/modules/Nav/Nav.tsx
+++ src/modules/Nav/Nav.tsx
@@ -1,48 +1,57 @@
 import { FluidContainer, Image } from 'components';
+import styled from 'styled-components';
 import Link from 'next/link';
 import { useBreakpoint } from 'hooks';
 import { MobileNav } from './MobileNav';
 import { DesktopNav } from './DesktopNav';
 import { Search } from 'modules/Search';
 
+const NavWrapper = styled.div`
+  filter: drop-shadow(0px 2px 4px rgb(0, 0, 0, 0.1));
+`;
+
 export const Nav = () => {
   const { isMini, isMobile, isTablet } = useBreakpoint();
   return isTablet ? (
-    <FluidContainer
-      padding="24px"
-      backgroundColor="white"
-      justifyContent="space-between"
-      alignItems="center"
-      flex
-    >
-      <Link href="/">
-        <Image
-          tabIndex={0}
-          maxHeight={isMobile ? '64px' : '80px'}
-          src={isMini ? '/usu-logo-white.png' : '/usu-wordmark.png'}
-          alt="Cal State LA University-Student Union Logo"
-        />
-      </Link>
-      <MobileNav />
-    </FluidContainer>
+    <NavWrapper>
+      <FluidContainer
+        padding="24px"
+        backgroundColor="white"
+        justifyContent="space-between"
+        alignItems="center"
+        flex
+      >
+        <Link href="/">
+          <Image
+            tabIndex={0}
+            maxHeight={isMobile ? '64px' : '80px'}
+            src={isMini ? '/usu-logo-white.png' : '/usu-wordmark.png'}
+            alt="Cal State LA University-Student Union Logo"
+          />
+        </Link>
+        <MobileNav />
+      </FluidContainer>
+    </NavWrapper>
   ) : (
-    <FluidContainer
-      flex
-      justifyContent="space-between"
-      flexWrap="wrap"
-      alignItems="center"
-      padding="24px"
-      backgroundColor="greyDarkest"
-    >
-      <Link href="/">
-        <Image
-          maxWidth="200px"
-          src="/usu-wordmark-white.png"
-          alt="Cal State LA University-Student Union Logo"
-        />
-      </Link>
-      <DesktopNav />
-      <Search />
-    </FluidContainer>
+    <NavWrapper>
+      <FluidContainer
+        flex
+        justifyContent="space-between"
+        flexWrap="wrap"
+        alignItems="center"
+        padding="24px"
+        backgroundColor="greyDarkest"
+      >
+        <Link href="/">
+          <Image
+            maxWidth="200px"
+            src="/usu-wordmark-white.png"
+            alt="Cal State LA University-Student Union Logo"
+          />
+        </Link>
+        <DesktopNav />
+        <Search />
+      </FluidContainer>
+    </NavWrapper>
   );
 };
