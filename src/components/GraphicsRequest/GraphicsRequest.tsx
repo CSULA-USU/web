3a920098e794899ce2967f36cd204d9029548217
tammyxa <icecoldxD123@gmail.com>
2023-12-12 15:52:14 -0800

    feat: color status buttons; put requests in expandable

--- src/components/GraphicsRequest/GraphicsRequest.tsx
+++ src/components/GraphicsRequest/GraphicsRequest.tsx
@@ -1,27 +1,105 @@
 import { useEffect, useState } from 'react';
-import { Button, FluidContainer, Image, Typography } from 'components';
+import {
+  Button,
+  Expandable,
+  FluidContainer,
+  Image,
+  Typography,
+} from 'components';
 import { Header, Page } from 'modules';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { graphicsRequestListState } from 'atoms';
 import { useRecoilValue } from 'recoil';
 import graphicsRequests from 'data/graphics-requests.json';
 import departments from 'data/departments.json';
+import { BiChevronRight } from 'react-icons/bi';
+import { Spaces } from 'theme';
 
 export interface Prop {
   department: 'csi' | 'ccc' | 'graffix' | 'operations' | 'recreation';
 }
 
-const StatusButton = styled.button`
+interface StatusButtonProps {
+  color?:
+    | 'grey'
+    | 'orange'
+    | 'purple'
+    | 'blue'
+    | 'pink'
+    | 'red'
+    | 'green'
+    | 'brown';
+}
+
+const getNotionColors = (color: StatusButtonProps['color']) => {
+  switch (color) {
+    case 'grey':
+      return css`
+        background-color: #787774;
+        color: #fff;
+      `;
+    case 'orange':
+      return css`
+        background-color: #d9730d;
+        color: #fff;
+      `;
+    case 'purple':
+      return css`
+        background-color: #9065b0;
+        color: #fff;
+      `;
+    case 'blue':
+      return css`
+        background-color: #337ea9;
+        color: #fff;
+      `;
+    case 'pink':
+      return css`
+        background-color: #c14c8a;
+        color: #fff;
+      `;
+    case 'red':
+      return css`
+        background-color: #d44c47;
+        color: #fff;
+      `;
+    case 'green':
+      return css`
+        background-color: #448361;
+        color: #fff;
+      `;
+    case 'brown':
+      return css`
+        background-color: #9f6b53;
+        color: #fff;
+      `;
+    default:
+      return css`
+        background-color: #37352f;
+        color: #fff;
+      `;
+  }
+};
+
+const StatusButton = styled.button<StatusButtonProps>`
   text-align: center;
   border: none;
   border-radius: 5px;
-  margin: 1%;
+  margin: ${Spaces.md};
+
+  ${(props) => getNotionColors(props.color)}
 `;
 
 const StatusNav = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const ExpandableContainer = styled.div`
+  border: 1px solid;
+  padding-left: ${Spaces.md};
+  margin: ${Spaces.md};
 `;
 
 const Table = styled.table`
@@ -58,7 +136,7 @@ export const GraphicsRequests = ({}: Prop) => {
   return (
     <Page>
       <Header
-        title={`${department} Graphics Request`}
+        title={`${department?.title} Graphics Request`}
         backgroundImage="/subtle-background-2.jpg"
       >
         <Image
@@ -68,57 +146,73 @@ export const GraphicsRequests = ({}: Prop) => {
         ></Image>
       </Header>
       <StatusNav>
-        <StatusButton color="">All</StatusButton>
-        <StatusButton>Not Started</StatusButton>
-        <StatusButton>In-Progress</StatusButton>
-        <StatusButton>Approved</StatusButton>
-        <StatusButton>Send to Print</StatusButton>
-        <StatusButton>Waiting for Approval</StatusButton>
-        <StatusButton>On Hold</StatusButton>
-        <StatusButton>Complete</StatusButton>
-        <StatusButton>Cancelled</StatusButton>
+        <StatusButton>All</StatusButton>
+        <StatusButton color="grey">Not Started</StatusButton>
+        <StatusButton color="orange">In-Progress</StatusButton>
+        <StatusButton color="purple">Approved</StatusButton>
+        <StatusButton color="blue">Send to Print</StatusButton>
+        <StatusButton color="pink">Waiting for Approval</StatusButton>
+        <StatusButton color="red">On Hold</StatusButton>
+        <StatusButton color="green">Complete</StatusButton>
+        <StatusButton color="brown">Cancelled</StatusButton>
       </StatusNav>
       <FluidContainer>
-        {requests.map((item) => (
-          <FluidContainer key={item.title}>
-            <Typography variant="labelTitle" color="gold">
-              {item.submissionDate}
-            </Typography>
-            <Typography variant="pageHeader" size="lg">
-              {item.title}
-            </Typography>
-            <Table>
-              <tr>
-                <td>
-                  <span>Requestor</span>: {item.requestor}
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <span>Artist</span>: {item.artist}
-                </td>
-              </tr>
-            </Table>
+        {requests.map((event) => (
+          <ExpandableContainer key={event.title}>
+            <Expandable
+              indicator={<BiChevronRight color="black" size={48} />}
+              header={
+                <Typography
+                  variant="label"
+                  color="black"
+                  as="h3"
+                  margin={`${Spaces.sm} 0`}
+                >
+                  {event.title}
+                </Typography>
+              }
+            >
+              <Table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <span>Submission Date</span>: {event.submissionDate}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <span>Requestor</span>: {event.requestor}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <span>Artist</span>: {event.artist}
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
 
-            <Table>
-              <tr>
-                <th>Digital Delivery:</th>
-                <th>Send to Print:</th>
-                <th>Print Delivery:</th>
-                <th>Event Date:</th>
-              </tr>
-              <tr>
-                <td>{item.digitalDelivery}</td>
-                <td>{item.printDate}</td>
-                <td>{item.printDelivery}</td>
-                <td>{item.eventDate}</td>
-              </tr>
-            </Table>
-            <FluidContainer flex justifyContent="center">
-              <Button variant="black">View Request</Button>
-            </FluidContainer>
-            <hr></hr>
-          </FluidContainer>
+              <Table>
+                <tbody>
+                  <tr>
+                    <th>Digital Delivery:</th>
+                    <th>Send to Print:</th>
+                    <th>Print Delivery:</th>
+                    <th>Event Date:</th>
+                  </tr>
+                  <tr>
+                    <td>{event.digitalDelivery}</td>
+                    <td>{event.printDate}</td>
+                    <td>{event.printDelivery}</td>
+                    <td>{event.eventDate}</td>
+                  </tr>
+                </tbody>
+              </Table>
+              <FluidContainer flex justifyContent="center">
+                <Button variant="primary">View Request</Button>
+              </FluidContainer>
+            </Expandable>
+          </ExpandableContainer>
         ))}
       </FluidContainer>
     </Page>
---
+++ src/pages/graffix/backoffice/csi.tsx
@@ -0,0 +1,5 @@
+import { GraphicsRequests } from 'components';
+
+export default function CCC() {
+  return <GraphicsRequests department="csi"></GraphicsRequests>;
+}
