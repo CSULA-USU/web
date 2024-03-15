c4bd8bbdd212e1a6d8f227880a90928375236054
John Yasis <johnamielyasis@gmail.com>
2024-03-15 16:03:39 -0700

    chore: differentiate between different gene cards, update icon choice, create widescreen breakpoint

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -18,20 +18,20 @@ export const Icon = ({ iconName, size }: IconProps): ReactElement => {
   // Define an object to map icon names to their corresponding components
   const iconComponents: { [key: string]: ReactElement } = {
     BiCoffeeTogo: createElement(BiCoffeeTogo, { size }),
-    GiRollerSkate: createElement(GiRollerSkate, { size }),
-    MdSportsBasketball: createElement(MdSportsBasketball, { size }),
-    FaRunning: createElement(FaRunning, { size }),
-    GiBabyBottle: createElement(GiBabyBottle, { size }),
-    FaMountainSun: createElement(FaMountainSun, { size }),
     BsSignpost2Fill: createElement(BsSignpost2Fill, { size }),
     CgPokemon: createElement(CgPokemon, { size }),
     FaGamepad: createElement(FaGamepad, { size }),
+    FaMountainSun: createElement(FaMountainSun, { size }),
     FaPaperPlane: createElement(FaPaperPlane, { size }),
     FaPlateWheat: createElement(FaPlateWheat, { size }),
+    FaRunning: createElement(FaRunning, { size }),
+    GiBabyBottle: createElement(GiBabyBottle, { size }),
+    GiRollerSkate: createElement(GiRollerSkate, { size }),
     GiStrawberry: createElement(GiStrawberry, { size }),
     ImSpoonKnife: createElement(ImSpoonKnife, { size }),
     LuCakeSlice: createElement(LuCakeSlice, { size }),
     LuSoup: createElement(LuSoup, { size }),
+    MdSportsBasketball: createElement(MdSportsBasketball, { size }),
   };
 
   // Render the corresponding icon component based on the iconName prop
