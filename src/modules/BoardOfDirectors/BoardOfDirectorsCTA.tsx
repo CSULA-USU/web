2b2c90b21609283fe4b469a86f0f28af6a57db57
John Yasis <johnamielyasis@gmail.com>
2024-04-10 16:52:30 -0700

    chore: change padding settings for BOD CTA in homepage

--- src/modules/BoardOfDirectors/BoardOfDirectorsCTA.tsx
+++ src/modules/BoardOfDirectors/BoardOfDirectorsCTA.tsx
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 import { Button, Typography } from 'components';
+import { useBreakpoint } from 'hooks';
 
-const BoardOfDirectorsCTAContainer = styled.div`
+// Define interface for props
+interface BoardOfDirectorsCTAContainerProps {
+  isMobile: boolean;
+}
+
+const BoardOfDirectorsCTAContainer = styled.div<BoardOfDirectorsCTAContainerProps>`
   background-image: url('/backgrounds/subtle-background-4.jpg');
   background-position: center;
   background-size: cover;
-  padding: 240px 72px;
+  padding: ${(props) => (props.isMobile ? '60px 16px' : '120px 72px')};
 `;
 
 const BoardOfDirectorsCTAText = styled.div`
@@ -25,26 +31,33 @@ const BoardOfDirectorsCTAButtons = styled.div`
   column-gap: 16px;
   row-gap: 16px;
 `;
-export const BoardOfDirectorsCTA = () => (
-  <BoardOfDirectorsCTAContainer>
-    <BoardOfDirectorsCTAText>
-      <Typography as="h2" variant="title" size="3xl">
-        Board of Directors
-      </Typography>
-      <Typography margin="revert-layer">
-        Apply to be on the board of directors to make a difference while
-        developing your leadership skills.
-      </Typography>
-    </BoardOfDirectorsCTAText>
-    <BoardOfDirectorsCTAButtons>
-      <abbr title="University Student Union Board of Directors Application">
-        <Button variant="black" href="https://form.jotform.com/240387162166154">
-          U-SU Board of Directors Application
+export const BoardOfDirectorsCTA = () => {
+  const { isMobile } = useBreakpoint();
+
+  return (
+    <BoardOfDirectorsCTAContainer isMobile={isMobile}>
+      <BoardOfDirectorsCTAText>
+        <Typography as="h2" variant="title" size="3xl">
+          Board of Directors
+        </Typography>
+        <Typography margin="revert-layer">
+          Apply to be on the board of directors to make a difference while
+          developing your leadership skills.
+        </Typography>
+      </BoardOfDirectorsCTAText>
+      <BoardOfDirectorsCTAButtons>
+        <abbr title="University Student Union Board of Directors Application">
+          <Button
+            variant="black"
+            href="https://form.jotform.com/240387162166154"
+          >
+            U-SU Board of Directors Application
+          </Button>
+        </abbr>
+        <Button variant="outline" href="/governance/meet-the-board">
+          View Current Board Members
         </Button>
-      </abbr>
-      <Button variant="outline" href="/governance/meet-the-board">
-        View Current Board Members
-      </Button>
-    </BoardOfDirectorsCTAButtons>
-  </BoardOfDirectorsCTAContainer>
-);
+      </BoardOfDirectorsCTAButtons>
+    </BoardOfDirectorsCTAContainer>
+  );
+};
