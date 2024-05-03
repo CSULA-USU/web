0a8bd8dc4e72ac376b9d81cd728efd862b077673
John Yasis <johnamielyasis@gmail.com>
2024-05-02 17:09:22 -0700

    chore: connect photos to native grad participant acknowledgment page

--- src/pages/api/jotform.ts
+++ src/pages/api/jotform.ts
@@ -48,6 +48,7 @@ export default async function handler(
             major: submission.major,
             pronouns: submission.tribe,
             acknowledgement: submission.acknowledgement,
+            img: submission.img,
           };
           specificDataObj.push(person);
         });
