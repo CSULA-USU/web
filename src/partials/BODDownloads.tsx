671e4945a5a11415c478f1c3f53f4cb81b707f6a
John Yasis <johnamielyasis@gmail.com>
2024-10-21 10:01:29 -0700

    chore: upload new agendas for oct 25

--- src/partials/BODDownloads.tsx
+++ src/partials/BODDownloads.tsx
@@ -21,27 +21,35 @@ const agendaDownloads: DownloadSectionProps[] = [
         stacked
         links={[
           {
-            href: 'https://www.dropbox.com/scl/fi/9swbxe510wvl5ppttcyvc/Agenda-U-SU-Board-of-Directors-August-30-2024.docx?rlkey=90ttu2o7qmu1zeiqt7fpdfyzn&dl=0',
+            href: 'https://www.dropbox.com/scl/fi/9swbxe510wvl5ppttcyvc/Agenda-U-SU-Board-of-Directors-August-30-2024.docx?rlkey=90ttu2o7qmu1zeiqt7fpdfyzn&raw=1',
             children: 'Meeting August 30, 2024',
           },
           {
-            href: 'https://www.dropbox.com/scl/fi/4bffzfcit7fmu4nkj8aby/Agenda-Fiscal-Committee-September-13-2024.docx?rlkey=nsh58bdlv6z32v5cze7laok2k&st=1c15fbgm&dl=0',
+            href: 'https://www.dropbox.com/scl/fi/4bffzfcit7fmu4nkj8aby/Agenda-Fiscal-Committee-September-13-2024.docx?rlkey=nsh58bdlv6z32v5cze7laok2k&st=1c15fbgm&raw=1',
             children: 'Fiscal Committee September 13, 2024',
           },
           {
-            href: 'https://www.dropbox.com/scl/fi/sv5wydj0gop9wtbbgtx5k/Agenda-Audit-Committee-September-13-2024.docx?rlkey=gywniga9axihrk9uhldlvpr6x&st=jp1e5atu&dl=0',
+            href: 'https://www.dropbox.com/scl/fi/sv5wydj0gop9wtbbgtx5k/Agenda-Audit-Committee-September-13-2024.docx?rlkey=gywniga9axihrk9uhldlvpr6x&st=jp1e5atu&raw=1',
             children: 'Audit Committee September 13, 2024',
           },
           {
-            href: 'https://www.dropbox.com/scl/fi/hgwh281cms1qw6fnwzktw/Agenda-U-SU-Board-of-Directors-September-20-2024.docx?rlkey=8q3xoiagc81m1y19b2m7gvmet&dl=0',
+            href: 'https://www.dropbox.com/scl/fi/hgwh281cms1qw6fnwzktw/Agenda-U-SU-Board-of-Directors-September-20-2024.docx?rlkey=8q3xoiagc81m1y19b2m7gvmet&raw=1',
             children: 'Meeting September 20, 2024',
+          },
+          {
+            href: 'https://www.dropbox.com/scl/fi/7dv75v6rxqb2atdrpdspe/Agenda-Board-of-Directors-October-25-2024.docx?rlkey=95jru0v5leqm3njjpoddat6gd&st=sga0hzg0&raw=1',
+            children: 'Meeting October 25, 2024',
+          },
+          {
+            href: 'https://www.dropbox.com/scl/fi/o5kfe6rt5y8y08nypjt1e/Agenda-Ad-hoc-Bylaws-Committee-October-25-2024.docx?rlkey=0294tig72y3tvu7nendd40qq8&st=qu06ylot&raw=1',
+            children: 'Ad-hoc Bylaws Committee October 25, 2024',
           },
         ]}
       />
     ),
     button: {
       children: <NonBreakingSpan>Download All</NonBreakingSpan>,
-      href: 'https://www.dropbox.com/scl/fi/c7lxj8214251r9c4rtzc6/Archive.zip?rlkey=zox1d8b3v5q57ku0x3gj3dq99&dl=0',
+      href: 'https://www.dropbox.com/scl/fi/c7lxj8214251r9c4rtzc6/Archive.zip?rlkey=zox1d8b3v5q57ku0x3gj3dq99&raw=1',
       variant: 'black',
     },
   },
@@ -55,7 +63,7 @@ const minutesDownloads: DownloadSectionProps[] = [
         stacked
         links={[
           {
-            href: 'https://www.dropbox.com/scl/fi/p95bxqarp97fy5ju737z4/Minutes-Board-of-Directors-August-30-2024-signed.pdf?rlkey=40neyyje6c1bwno80hdquz798&st=toduyfkj&dl=0',
+            href: 'https://www.dropbox.com/scl/fi/p95bxqarp97fy5ju737z4/Minutes-Board-of-Directors-August-30-2024-signed.pdf?rlkey=40neyyje6c1bwno80hdquz798&st=toduyfkj&raw=1',
             children: 'Board of Directors August 30, 2024',
           },
         ]}
@@ -63,7 +71,7 @@ const minutesDownloads: DownloadSectionProps[] = [
     ),
     button: {
       children: <NonBreakingSpan>Download All</NonBreakingSpan>,
-      href: 'https://www.dropbox.com/scl/fi/p95bxqarp97fy5ju737z4/Minutes-Board-of-Directors-August-30-2024-signed.pdf?rlkey=40neyyje6c1bwno80hdquz798&st=toduyfkj&dl=0',
+      href: 'https://www.dropbox.com/scl/fi/p95bxqarp97fy5ju737z4/Minutes-Board-of-Directors-August-30-2024-signed.pdf?rlkey=40neyyje6c1bwno80hdquz798&st=toduyfkj&raw=1',
       variant: 'black',
     },
   },
@@ -84,7 +92,7 @@ export const BODDownloads = () => (
       title="Fiscal Year 24-25"
       button={{
         children: <NonBreakingSpan>See Schedule</NonBreakingSpan>,
-        href: 'https://www.dropbox.com/scl/fi/bnxxquo4d6hob0jp45avu/U-SU-2024-25-BOD-Meeting-Schedule.xlsx.pdf?rlkey=thadfs2lylcbeaq82ujuhsqib&dl=0',
+        href: 'https://www.dropbox.com/scl/fi/bnxxquo4d6hob0jp45avu/U-SU-2024-25-BOD-Meeting-Schedule.xlsx.pdf?rlkey=thadfs2lylcbeaq82ujuhsqib&raw=1',
         variant: 'black',
       }}
     />
