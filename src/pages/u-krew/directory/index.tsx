693c1b25ee80d395061b889a72ff61f295fccb63
Pickolzi <pickol876@gmail.com>
2025-01-22 13:07:48 -0800

    chore: Fill U-Krew directory page with data, temporary design

--- src/pages/u-krew/directory/index.tsx
+++ src/pages/u-krew/directory/index.tsx
@@ -1,7 +1,29 @@
 import axios from 'axios';
+import {
+  TabCluster,
+  FluidContainer,
+  StaffCardWithModal,
+  Typography,
+} from 'components';
+import { Page } from 'modules';
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
+import { TabPanel } from 'react-tabs';
+import { Spaces } from 'theme';
 import { UKrewStudent } from 'types';
+import { toTitleCase } from 'utils/stringhelpers';
 // import { Image } from 'components';
+
+const departmentTabData = [
+  'All',
+  'Directors',
+  'Administration',
+  'CSI',
+  'CCC',
+  'Graffix',
+  'Operations',
+  'Recreation',
+];
 
 export default function Directory() {
   const [uKrewStudents, setUKrewStudents] = useState<
@@ -18,26 +40,59 @@ export default function Directory() {
   }, []);
 
   return (
-    <>
-      Hello Students!
-      {uKrewStudents?.map((uKrewStudent: UKrewStudent) => {
-        return (
-          <div key={uKrewStudent.email}>
-            {/* <Image src={uKrewStudent.photoUpload || ''} width="300px"></Image> */}
-            <p>First Name: {uKrewStudent.firstName}</p>
-            <p>Last Name: {uKrewStudent.lastName}</p>
-            <a href={uKrewStudent.email?.split('@')[0]}>
-              Student Card: {uKrewStudent.email?.split('@')[0]}
-            </a>
-            <p>Email: {uKrewStudent.email}</p>
-            <p>Department: {uKrewStudent.department}</p>
-            <p>Major: {uKrewStudent.major}</p>
-            <p>Phone Number: {uKrewStudent.phoneNumber}</p>
-            <p>Session: {uKrewStudent.session}</p>
-            <hr />
-          </div>
-        );
-      })}
-    </>
+    <Page>
+      <Head>
+        <title>U-SU U-Krew Directory</title>
+        <meta name="author" content="University-Student Union, Cal State LA" />
+        <meta
+          name="keywords"
+          content="Cal State LA, CSULA, U-SU, University-Student Union, Staff, Directors, Administration, Center for Student Involvement, CSI, Cross Cultural Centers, CCC, Graffix, Operations, Recreation, Coordinator, Dean, Technician, Assistant, Web Designer, Executive, Employee"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <TabCluster tabItems={departmentTabData}>
+        {departmentTabData.map((tab) => (
+          <TabPanel key={tab}>
+            <FluidContainer flex flexWrap="wrap" justifyContent="center">
+              {uKrewStudents?.map(
+                (uKrewStudent, idx) =>
+                  (tab === 'All' || uKrewStudent.department.includes(tab)) && (
+                    <StaffCardWithModal
+                      key={idx}
+                      name={`${toTitleCase(
+                        uKrewStudent.firstName,
+                      )} ${toTitleCase(uKrewStudent.lastName)}`}
+                      title={
+                        uKrewStudent.role
+                          ? uKrewStudent.role
+                          : uKrewStudent.department.includes('Directors')
+                          ? 'Board Of Directors'
+                          : ''
+                      }
+                      // src={uKrewStudent.photoUpload || ''}
+                      src="https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg"
+                      alt={''}
+                      // tags={uKrewStudent.tags}
+                      margin={`${Spaces.sm}`}
+                      // pronouns={uKrewStudent.pronouns}
+                      email={uKrewStudent.email}
+                      phone={uKrewStudent.phoneNumber}
+                      // bio={uKrewStudent.bio}
+                      rounded
+                    >
+                      <Typography size="xs" as="p">
+                        {uKrewStudent.department}
+                      </Typography>
+                      <Typography size="xs" as="p">
+                        {uKrewStudent.email}
+                      </Typography>
+                    </StaffCardWithModal>
+                  ),
+              )}
+            </FluidContainer>
+          </TabPanel>
+        ))}
+      </TabCluster>
+    </Page>
   );
 }
