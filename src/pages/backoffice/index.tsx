e62d9a99e420b7a92a60aef1ea1ebbf52e1e0c7f
Pickolzi <pickol876@gmail.com>
2025-02-13 14:40:44 -0800

    Add Authentication using next-auth, including Microsoft's Active Directory's Outlook provider

--- src/pages/backoffice/index.tsx
+++ src/pages/backoffice/index.tsx
@@ -1,14 +1,34 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
+
+import { Button, FluidContainer, Typography } from 'components';
 import { Page } from 'modules';
 import Head from 'next/head';
 
 export default function Backoffice() {
+  const { data: session } = useSession();
+
   return (
     <Page>
       <Head>
         <title>U-SU Backoffice</title>
         <meta name="robots" content="noindex,nofollow" />
       </Head>
-      Hello Backoffice!
+      <FluidContainer>
+        <Typography as="h2" variant="titleLarge">
+          Hello Backoffice!
+        </Typography>
+        {session ? (
+          <>
+            <Typography>Welcome {session?.user?.email}</Typography>
+            <Button onClick={() => signOut()}>Sign Out</Button>
+          </>
+        ) : (
+          <>
+            <Typography>Please Sign in to continue.</Typography>
+            <Button onClick={() => signIn()}>Sign in!</Button>
+          </>
+        )}
+      </FluidContainer>
     </Page>
   );
 }
