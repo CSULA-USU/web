c48f633654a8229758092effc36b4c877e6ae7f9
John Yasis <johnamielyasis@gmail.com>
2025-06-23 11:48:25 -0700

    chore: styling updates for archives section makes it neater

--- src/modules/DocumentLink/DocumentLinkContainer.tsx
+++ src/modules/DocumentLink/DocumentLinkContainer.tsx
@@ -2,21 +2,35 @@ import styled from 'styled-components';
 import { DocumentLinkProps, DocumentLink } from './DocumentLink';
 
 export interface DocumentLinkContainerProps {
+  grid?: boolean;
   links: DocumentLinkProps[];
   stacked?: boolean;
 }
 
-const LinkContainer = styled.div<{ stacked: boolean }>`
+const LinkContainer = styled.div<{ stacked: boolean; grid: boolean }>`
   display: flex;
   flex-wrap: wrap;
+  width: 100%;
   ${(p) => p.stacked && `flex-direction: column;`}
+  ${(p) =>
+    p.grid &&
+    `display: grid; 
+     grid-template-columns: repeat(3, 1fr);
+     @media (max-width: 1428px) {
+      grid-template-columns: repeat(2, 1fr);
+     }
+     @media (max-width: 1081px) {
+      grid-template-columns: 1fr;
+     }
+     `}
 `;
 
 export const DocumentLinkContainer = ({
   links,
   stacked,
+  grid,
 }: DocumentLinkContainerProps) => (
-  <LinkContainer stacked={!!stacked}>
+  <LinkContainer stacked={!!stacked} grid={!!grid}>
     {links.map((l, i) => (
       <DocumentLink key={`${l.children}_${i}`} {...l} />
     ))}
