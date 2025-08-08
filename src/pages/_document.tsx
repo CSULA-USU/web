dda100c6c85ca9912dc8e5b1788932f428df68a3
John Yasis <johnamielyasis@gmail.com>
2025-08-08 14:20:02 -0700

    fix: workaround for nextjs and styled-components SSR pattern

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,45 @@
 // pages/_document.js
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        (originalRenderPage as any)({
+          enhanceApp: (App: any) =>
+            function EnhancedApp(props: any) {
+              return sheet.collectStyles(<App {...props} />);
+            },
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html lang="en">
@@ -33,5 +71,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
