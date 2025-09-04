3f1b3caa462d40a75ba1c665ca97a0bb6615b5ac
ivilla15 <124205432+ivilla15@users.noreply.github.com>
2025-09-04 08:46:37 -0700

    chore: update minutes pdf to optimized for screen readers

--- src/partials/BODDownloads.tsx
+++ src/partials/BODDownloads.tsx
@@ -44,7 +44,7 @@ const minutesDownloads: DownloadSectionProps[] = [
         stacked
         links={[
           {
-            href: 'https://www.dropbox.com/scl/fi/gv8tq2nquadg9rdwznm93/Board-of-Directors-May-9-2025.pdf?rlkey=fo5zxrwmspyho9lig2kydl0iq&st=92wa1lzz&raw=1',
+            href: 'https://www.dropbox.com/scl/fi/m61csa33yftdg1dg1pbjm/Board-of-Directors-May-9-2025.pdf?rlkey=ps67jkk1ft32a0romtbii9bn0&st=06aerr6n&raw=1',
             children: 'BOD May 9, 2025',
           },
         ]}
@@ -52,7 +52,7 @@ const minutesDownloads: DownloadSectionProps[] = [
     ),
     button: {
       children: <NonBreakingSpan>Download All</NonBreakingSpan>,
-      href: 'https://www.dropbox.com/scl/fi/u2axl3qac4r0yuy9my1um/Agenda-Board-of-Directors-August-22-2025.docx?rlkey=dp1tmxt65802vara3yspdpsqi&raw=1',
+      href: 'https://www.dropbox.com/scl/fi/m61csa33yftdg1dg1pbjm/Board-of-Directors-May-9-2025.pdf?rlkey=ps67jkk1ft32a0romtbii9bn0&st=06aerr6n&dl=1',
       variant: 'black',
     },
   },
