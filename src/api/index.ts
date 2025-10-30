9a083ace9fb2fe63e0a6507ed850e09853f5966b
ivilla15 <124205432+ivilla15@users.noreply.github.com>
2025-10-30 08:46:21 -0700

    feat: bod backoffice page completely done. Archive page and meeting schedule use bod backoffice

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,31 +12,54 @@ export * from './supabase';
 export const fetchEvents = async (
   setEventsStatus: SetterOrUpdater<StatusType>,
 ) => {
-  const res = await fetch('https://api.presence.io/calstatela/v1/events');
-  if (!res.ok) {
-    setEventsStatus('failed');
-    return [];
-  }
-  setEventsStatus('success');
-  return res.json();
+  const events = await fetch(
+    'https://api.presence.io/calstatela/v1/events',
+  ).then((res) => {
+    if (!res.ok) {
+      setEventsStatus('failed');
+      return [];
+    } else {
+      setEventsStatus('success');
+      return res.json();
+    }
+  });
+  return events;
 };
 
 export const fetchInstagramFeed = async (
   setInstagramResponseStatus: Dispatch<SetStateAction<StatusType>>,
   org: string,
 ) => {
-  const res = await fetch(`/api/instagram?org=${encodeURIComponent(org)}`);
-  if (!res.ok) {
-    setInstagramResponseStatus('failed');
-    return null;
-  }
-  setInstagramResponseStatus('success');
-  return res.json();
+  const data = await fetch(`/api/instagram?org=${org}`).then((res) => {
+    if (!res.ok) {
+      setInstagramResponseStatus('failed');
+      return null;
+    } else {
+      setInstagramResponseStatus('success');
+      return res.json();
+    }
+  });
+  return await data;
 };
 
 export const fetchRequests = async () => {
-  const res = await fetch('/api/notion');
-  return await res.json();
+  try {
+    const data = await fetch('/api/notion');
+    const requestFeed = await data.json();
+    return requestFeed;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+export const fetchJotform = async (id: any) => {
+  try {
+    const data = await fetch(`/api/jotform?id=${id}`);
+    const dataFeed = await data.json();
+    return dataFeed;
+  } catch (error) {
+    console.error('Error fetching jotform data:', error);
+  }
 };
 
 /* --------------------------- Meeting documents DB -------------------------- */
