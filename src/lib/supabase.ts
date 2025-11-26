42cf8c38e52a68d6a7c53bd4b0d367772b7fb342
ivilla15 <124205432+ivilla15@users.noreply.github.com>
2025-11-26 09:25:27 -0800

    feat: bod fixes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -6,7 +6,7 @@ import type { BackOfficeUser } from 'types';
 type Database = Omit<SupabaseSchema, '__InternalSupabase'>;
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
   throw new Error('Missing Supabase URL or Key');
